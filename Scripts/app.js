f4195b9dd1a72c32a52ec4a4ab0a5b79c50d93af
Johnnypham2 <phammjohnny@gmail.com>
2024-04-24 18:26:54 -0700

    stuff

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,22 +1,21 @@
-let joke = document.getElementById("joke")
-let jokeBtn = document.getElementById("jokeBtn")
-
+let joke = document.getElementById("joke");
+let jokeBtn = document.getElementById("jokeBtn");
 
 //joke function
-const getJokes = async() => {
-    const promise = await fetch("https://icanhazdadjoke.com/", {
-        headers:{
-            'Accept':'application/json'
-        }
-    });
+const getJokes = async () => {
+  const promise = await fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json",
+    },
+  });
 
-    const data = await promise.json();
-    console.log(data);
-    joke.textContent = data.joke;
-    return data;
-}
+  const data = await promise.json();
+  console.log(data);
+  joke.textContent = data.joke;
+  return data;
+};
 
 //added eventlisteners for your button click
-jokeBtn.addEventListener("click", async() =>{
-    await getJokes();
+jokeBtn.addEventListener("click", async () => {
+  await getJokes();
 });
